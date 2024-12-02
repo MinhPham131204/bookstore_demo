0fbdeed3cfd506e105fd417eec13db8f7bcdc979
minh <pqminh131204@gmail.com>
2024-12-02 21:41:59 +0700

    update_discount

--- backend/routing/seller/discount.js
+++ backend/routing/seller/discount.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const DiscountController = require('../../controller/seller/discount')
 
-router.get('/all', async (req, res) => {
-    DiscountController.showDiscountList
+router.get('/', async (req, res) => {
+    const result = await DiscountController.showDiscountList()
+    res.json(result)
 })
 
 module.exports = router
