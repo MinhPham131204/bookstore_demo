a2e4b0acadbca502a1c8151bf587ea9ab82569d9
unknown <huynhductai1905@gmail.com>
2024-12-03 14:02:22 +0700

    Add 2 new module to index.js

--- backend/routing/customer/index.js
+++ backend/routing/customer/index.js
@@ -1,8 +1,11 @@
 const changeInfo = require('./changeInfo');
-
+const orderList = require('./order');
+const Password = require('./password');
 
 function route(app){
     app.use('/Info', changeInfo);
+    app.use('/order', orderList);
+    app.use('/password', Password);
 }
 
 module.exports = route;
