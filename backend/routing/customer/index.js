19fb14e9e271a5e04b1c5b59cce77512f44e851a
minh <pqminh131204@gmail.com>
2024-12-07 00:28:24 +0700

    no_login

--- backend/routing/customer/index.js
+++ backend/routing/customer/index.js
@@ -5,22 +5,22 @@ const cartRouter = require('./cart')
 const resetPass = require('./password')
 const changeInfo = require('./changeInfo')
 
-function requireLogin(req, res, next) {
-    if (req.cookies.userID) {
-        next();
-    }
+// function requireLogin(req, res, next) {
+//     if (req.cookies.userID) {
+//         next();
+//     }
 
-    else res.redirect('/login')
-}
+//     else res.redirect('/login')
+// }
 
 function route(app) {
 
     app.use('/login', loginRouter)
-    app.use('/main-page', requireLogin, bookRouter)
-    app.use('/order', requireLogin, orderRouter)
-    app.use('/cart', requireLogin, cartRouter)
-    app.use('/forgotPassword', requireLogin, resetPass);
-    app.use('/info', requireLogin, changeInfo)
+    app.use('/main-page', bookRouter)
+    app.use('/order', orderRouter)
+    app.use('/cart', cartRouter)
+    app.use('/forgotPassword', resetPass);
+    app.use('/info', changeInfo)
 }
 
 module.exports = route;
